7f271352da1b0c70a1542cd7c2171cc7563c6820
bella.chen@baruchmail.cuny.edu <bella.chen@baruchmail.cuny.edu>
2024-11-25 21:40:23 +0000

    comit

--- models/model.sql
+++ models/model.sql
@@ -1,28 +1,24 @@
-{{ config(materialized='table') }}
+{{ config(materialized="table") }}
 
-with location as (
-    select
-        unique_key as id,
-        borough,
-        incident_zip as zip_code,
-        city
-    from `cis-4400-semester-project.dataset.311-service-requests`
-),
-complaint_type as (
-    select
-        unique_key as id,
-        complaint_type
-    from `cis-4400-semester-project.dataset.311-service-requests`
-),
-service_request as (
-    select
-        location.id,
-        complaint_type.complaint_type,
-        location.city,
-        location.borough,
-        location.zip_code
-    from location
-    join complaint_type using (id)
-)
+with
+    location as (
+        select unique_key as id, borough, incident_zip as zip_code, city
+        from `cis-4400-semester-project.dataset.311-service-requests`
+    ),
+    complaint_type as (
+        select unique_key as id, complaint_type
+        from `cis-4400-semester-project.dataset.311-service-requests`
+    ),
+    service_request as (
+        select
+            location.id,
+            complaint_type.complaint_type,
+            location.city,
+            location.borough,
+            location.zip_code
+        from location
+        join complaint_type using (id)
+    )
 
-select * from service_request
+select *
+from service_request
