a9426fe84f2007f4efb26cab48f86e343234018c
bella.chen@baruchmail.cuny.edu <bella.chen@baruchmail.cuny.edu>
2024-11-26 03:46:53 +0000

    c

---
+++ models/test.sql
@@ -0,0 +1,12 @@
+with
+    facts_housing_maintainence as (
+        select * from {{ ref("facts_housing_maintainence") }}
+    ),
+    facts_service_request as (select * from {{ ref("facts_service_request") }}),
+    intergrated as (
+        select distinct *
+        from facts_housing_maintainence fhm
+        inner join facts_service_request fsr on fsr.request_key = fhm.maintain_key
+    )
+select *
+from intergrated
