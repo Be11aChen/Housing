c870104e646af4c460aee0a6b5a2723ff4a4f4c2
bella.chen@baruchmail.cuny.edu <bella.chen@baruchmail.cuny.edu>
2024-11-26 05:26:31 +0000

    c

--- models/FactsTable/facts_housing_maintainence.sql
+++ models/FactsTable/facts_housing_maintainence.sql
@@ -1,15 +1,15 @@
 select row_number() over () as maintain_key, *
 from
     (
-        select hm_location_id, hm_complaint_type_id, hm_date_id, space_id, status_id
+        select hl.hm_location_id, hct.hm_complaint_type_id, hm_date_id, space_id, status_id
         from
-            {{ ref("dim_hm_complaint_type") }},
+            {{ ref("dim_hm_complaint_type") }} hct,
             {{ ref("dim_hm_date") }},
-            {{ ref("dim_hm_location") }},
+            {{ ref("dim_hm_location") }} hl,
             {{ ref("dim_space") }},
             {{ ref("dim_status") }}
         join
             {{ ref("facts_service_request") }} sr
-            on sr.complaint_type_id = complaint_type_id
-        join {{ ref("facts_service_request") }} sr2 on sr2.location_id = location_id
+            on sr.complaint_type_id = hct.hm_complaint_type_id
+        join {{ ref("facts_service_request") }} sr2 on sr2.location_id = hl.hm_location_id
     )
